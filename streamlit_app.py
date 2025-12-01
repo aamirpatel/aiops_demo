46e6cbb0a9918af08a074cac99f52041adfa46b9
Aamir Patel <paamir89@gmail.com>
2025-12-01 23:18:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
 
 import streamlit as st
 import psutil
-import pandas as pd
 
 st.title("AIOps Dashboard")
 
-# Metrics
 cpu = psutil.cpu_percent()
 ram = psutil.virtual_memory().percent
 disk = psutil.disk_usage('/').percent
